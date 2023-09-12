251770ae709243ac93cd659dc797ee41092b74dc
dashlin23 <142335984+dashlin23@users.noreply.github.com>
2023-09-13 00:24:06 +0100

    Add files via upload

---
+++ Debugging/CSharpFundamentals/CSharpFundamentals/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CSharpFundamentals
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            var numbers = new List<int> { 1, 2, 3, 4, 5, 6};
+            var smallests = GetSmallests(numbers, 3);
+
+            foreach (var number in smallests)
+                Console.WriteLine(number);
+        }
+
+        public static List<int> GetSmallests(List<int> list, int count)
+        {
+          
+
+            var buffer = new List<int>(list);
+            var smallests = new List<int>();
+
+            while (smallests.Count < count)
+            {
+                var min = GetSmallest(buffer);
+                smallests.Add(min);
+                buffer.Remove(min);
+            }
+
+            return smallests;
+        }
+
+        public static int GetSmallest(List<int> list)
+        {
+            // Assume the first number is the smallest
+            var min = list[0];
+            for (var i = 1; i < list.Count; i++)
+            {
+                if (list[i] < min)
+                    min = list[i];
+            }
+            return min;
+        }
+    }
+}
